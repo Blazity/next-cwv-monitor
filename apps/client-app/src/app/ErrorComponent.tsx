461680c695df7c4485496728e25de3d22604b1b1
Adrian Bielec <adrian.bielec@blazity.com>
2025-12-04 15:28:53 +0100

    CI: test handling client app, destroy build

---
+++ apps/client-app/src/app/ErrorComponent.tsx
@@ -0,0 +1,6 @@
+//This is component just for testing CI
+//CI should throw error because of eslint, and also run CI for this folder
+
+export const ErrorComponent: React.FC = () => {
+  return null;
+};
