ad22452fa43eccb58bf0f9ce40ef7f9ec5f1127f
Adrian Bielec <adrian.bielec@blazity.com>
2025-12-07 10:29:11 +0100

    CI: Fix pipeline, prepare for merge

--- packages/client-sdk/src/cwv-monitor.tsx
+++ packages/client-sdk/src/cwv-monitor.tsx
@@ -4,7 +4,7 @@ import { useMetrics } from './hooks/use-metrics.js';
 type Digit = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
 
 interface Props extends Omit<React.ComponentProps<typeof ConfigProvider>, 'children'> {
-  sampleRate: `0.${Digit}` | '1.0';
+  sampleRate?: `0.${Digit}` | '1.0';
   debug?: boolean;
   customDimensions?: unknown;
 }
