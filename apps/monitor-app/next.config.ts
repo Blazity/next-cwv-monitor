e2ffd0fb2c048b618d2305f696b6fbaa375bfb29
Adrian Bielec <adrian.bielec@blazity.com>
2025-12-19 10:42:20 +0100

    fix: Fix CI

--- apps/monitor-app/next.config.ts
+++ apps/monitor-app/next.config.ts
@@ -1,4 +1,3 @@
-import { env } from '@/env';
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
@@ -11,7 +10,7 @@ const nextConfig: NextConfig = {
         source: '/api/:path*',
         headers: [
           { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: env.CLIENT_APP_ORIGIN },
+          { key: 'Access-Control-Allow-Origin', value: process.env.CLIENT_APP_ORIGIN ?? '' },
           { key: 'Access-Control-Allow-Methods', value: 'GET,DELETE,PATCH,POST,PUT' },
           {
             key: 'Access-Control-Allow-Headers',
