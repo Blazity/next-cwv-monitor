85a8cda45002ca3062781971797b317ec8cb606d
Karol Chudzik <iipandarx@protonmail.com>
2025-12-27 18:21:06 +0100

    chore: adjust prettier config

--- apps/monitor-app/prettier.config.mjs
+++ apps/monitor-app/prettier.config.mjs
@@ -1,10 +1,7 @@
 /** @type {import("prettier").Config} */
 const config = {
-    singleQuote: true,
-    arrowParens: 'always',
-    trailingComma: 'none',
-    printWidth: 120,
-    tabWidth: 2,
-    plugins: ['prettier-plugin-tailwindcss']
-}
-export default config
+  tabWidth: 2,
+  plugins: ['prettier-plugin-tailwindcss']
+};
+
+export default config;
