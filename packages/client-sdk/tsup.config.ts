22a5ce569a7ce0a2483cd1787e38b379ce20f61d
Levosilimo <lev.sylin@gmail.com>
2025-12-29 11:53:36 +0100

    feat: change sdk target to ES2020, enforce use client directive, remove unused deps

--- packages/client-sdk/tsup.config.ts
+++ packages/client-sdk/tsup.config.ts
@@ -1,13 +1,32 @@
 import { defineConfig } from 'tsup';
+import fs from 'node:fs';
+import path from 'node:path';
 
-export default defineConfig({
-  entry: ['src/app-router.tsx', 'src/pages-router.tsx'],
-  format: ['cjs', 'esm'],
-  dts: true,
-  splitting: false,
-  clean: true,
-  sourcemap: true,
-  minify: true,
-  treeshake: true,
-  external: ['react', 'react-dom', 'next', 'next/*']
-});
+export default defineConfig([
+  {
+    entry: ['src/app-router.tsx', 'src/pages-router.tsx'],
+    format: ['cjs', 'esm'],
+    dts: true,
+    splitting: false,
+    clean: true,
+    sourcemap: true,
+    minify: true,
+    treeshake: true,
+    external: ['react', 'react-dom', 'next', 'next/*'],
+    // Use onSuccess to bypass Rollup's directive-stripping logic on treeshake
+    async onSuccess() {
+      const files = ['dist/app-router.js', 'dist/app-router.cjs'];
+
+      for (const file of files) {
+        const filePath = path.resolve(process.cwd(), file);
+        if (fs.existsSync(filePath)) {
+          const content = fs.readFileSync(filePath, 'utf8');
+          if (!content.startsWith('"use client"')) {
+            fs.writeFileSync(filePath, `"use client";\n${content}`);
+          }
+        }
+      }
+    },
+    noExternal: ['web-vitals']
+  }
+]);
